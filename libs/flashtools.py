e7f1648940c1dc9a5fca8a3963e9f20189f9d89b
dfangulo <58793508+dfangulo@users.noreply.github.com>
2023-01-18 15:27:54 -0700

    Areglando el formato para mejor lectura

--- libs/flashtools.py
+++ libs/flashtools.py
@@ -3,9 +3,9 @@ import shutil
 from libs import conn_db
 from smb.SMBConnection import SMBConnection
 
-#variables>
+# variables>
 info_local_files = {
-    'flashtool_path' : '.\\flashtool\\'
+    'flashtool_path': '.\\flashtool\\'
 }
 info_server = {
     'server': {
@@ -15,9 +15,10 @@ info_server = {
     'username': 'oa3admin',
     'password': 'OA#2020$',
     'share': 'server',
-    'flashtool_path' : '\\flashtool\\'
-    #\\192.168.2.100\server\flashtool
+    'flashtool_path': '\\flashtool\\'
+    # \\192.168.2.100\server\flashtool
 }
+
 
 # Funcion para agregar una nueva flash_tool
 def agregar_flash_tool(flash_id, nombre, ejecutable, ruta_folder, grabar_llave, borrar_llave):
@@ -31,8 +32,8 @@ def agregar_flash_tool(flash_id, nombre, ejecutable, ruta_folder, grabar_llave, 
     print(mycursor.rowcount, "Flash_tool insertada.")
     mydb.close()
 
-# Funcion para agregar una lsita de flashadores
-def agregar_flashadores(lista):
+
+def agregar_flashadores(lista):  # Funcion para agregar una lsita de flashadores
     for flashador in lista:
         flash_id, nombre, ejecutable, ruta_folder, grabar_llave, borrar_llave = flashador
         agregar_flash_tool(flash_id, nombre, ejecutable,
@@ -40,8 +41,7 @@ def agregar_flashadores(lista):
         print(f"Flashador {nombre} agregado.")
 
 
-# Funcion para borrar una flash_tool
-def borrar_flash_tool(flash_id):
+def borrar_flash_tool(flash_id):  # Funcion para borrar una flash_tool
     mydb = conn_db.conectar()
     mycursor = mydb.cursor()
     sql = "DELETE FROM flash_tools WHERE flash_id = %s"
@@ -51,8 +51,8 @@ def borrar_flash_tool(flash_id):
     print(mycursor.rowcount, "Flash_tool borrada.")
     mydb.close()
 
-# Funcion para mostrar todas las flash_tools
-def mostrar_flash_tools():
+
+def mostrar_flash_tools():  # Funcion para mostrar todas las flash_tools
     mydb = conn_db.conectar()
     mycursor = mydb.cursor()
     mycursor.execute("SELECT flash_id, nombre FROM flash_tools")
@@ -61,7 +61,8 @@ def mostrar_flash_tools():
         print(row[0] + ': ' + row[1])
     mydb.close()
 
-def obtener_ruta_folder(flash_id): #obtener el path de los flashadores 
+
+def obtener_ruta_folder(flash_id):  # obtener el path de los flashadores
     mydb = conn_db.conectar()
     mycursor = mydb.cursor()
     # Consulta para obtener la ruta_folder del flash_id especificado
@@ -72,19 +73,20 @@ def obtener_ruta_folder(flash_id): #obtener el path de los flashadores
     mydb.close()
     return ruta_folder
 
+
 def copy_folder_to_server(new_folder):
     source_folder_path = info_local_files['flashtool_path'] + new_folder
     destination_folder_path = '\\\\' + \
-    info_server['server']['ip'] + '\\' + \
-    info_server['share'] + info_server['flashtool_path']  + new_folder
+        info_server['server']['ip'] + '\\' + \
+        info_server['share'] + info_server['flashtool_path'] + new_folder
     try:
         conn = SMBConnection(info_server['username'], info_server['password'], "localhost",
                              info_server['server']['name'], use_ntlm_v2=True)
         conn.connect(info_server['server']['ip'], 139)
-        
+
         folder_name = os.path.basename(new_folder)
         print(folder_name)
-        if folder_name in [entry.filename for entry in conn.listPath(info_server['share'], info_server['flashtool_path'] )]:
+        if folder_name in [entry.filename for entry in conn.listPath(info_server['share'], info_server['flashtool_path'])]:
             print(
                 f"The folder {folder_name} already exists in {info_server['flashtool_path']}")
         else:
@@ -98,15 +100,19 @@ def copy_folder_to_server(new_folder):
     conn.close()
     return destination_folder_path
 
-def local_flashtool_folders(): # imprimir una lista de los flashadores en el folder ./flashtool
+
+def local_flashtool_folders():  # imprimir una lista de los flashadores en el folder ./flashtool
     folder_list = os.listdir(info_local_files['flashtool_path'])
     for folder in folder_list:
         print(folder)
-        
-def local_files_flashtool(folder): # listar los archivos para selecionar el ejecutable
+
+
+# listar los archivos para selecionar el ejecutable
+def local_files_flashtool(folder):
     dir_path = info_local_files['flashtool_path'] + folder
     if os.path.exists(dir_path):
-        files = [file for file in os.listdir(dir_path) if file.endswith(".exe")]
+        files = [file for file in os.listdir(
+            dir_path) if file.endswith(".exe")]
 
         if files:
             print("List of exe files:")
