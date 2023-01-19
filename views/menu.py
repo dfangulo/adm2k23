021d0ca244db58f5b78bfc6eaa93a9f9a1735d16
dfangulo <58793508+dfangulo@users.noreply.github.com>
2023-01-19 11:47:37 -0700

    Archivos modificados para la construccion del nuevo menu

--- views/menu.py
+++ views/menu.py
@@ -1,14 +1,29 @@
 from libs import tools
-def menu():
+
+def menu_top():
     tools.clear_terminal()
     print("\n   Administracion OA3 ver:1.0")
-    print("\n1. Agregar Orden de Produccion")
-    print("2. Agregar Series a la orden")
-    print("3. Consultar modelos")
-    print("4. Consultar flashadores")
-    print("5. Agregar modelo")
-    print("6. Agregar flashadores")
-    print("q. Salir")
+
+def menu_bottom():
     print('\n\tLanix Copyright© 2023')
     opcion = input("\nSelecciona una opción: ")
     return opcion
+
+def menu_opciones_principal():
+    print("\n1. Produccion")
+    print("2. Administracion")
+    print("3. Consultas")
+    print("4. Busquedas")
+    print("5. Ayuda")
+    print("q. Salir")
+
+def menu_produccion():
+    print("\n1. Agregar Orden de Produccion")
+    print("2. Agregar Series a la orden")
+    print("q. Salir")
+
+def menu_administrar():
+    print("\n1. Agregar Nuevas llaves")
+    print("2. Agregar Nuevos Modelos")
+    print("3. Agregar Nuevos Flashadores")
+    print("q. Salir")
