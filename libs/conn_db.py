8a43ab16426973f143e1160ac4748eb1719e41d9
David Angulo <dfangulo@gmail.com>
2023-01-28 12:35:30 -0700

    creatndo dos funciones para poder hacer pruebas en al base de datos en internett

--- libs/conn_db.py
+++ libs/conn_db.py
@@ -3,34 +3,22 @@ import mysql.connector
 
 def conectar():  # Crear una conexión a la base de datos
     mydb = mysql.connector.connect(
-<<<<<<< HEAD
         #db produccion
         host="oa3.lanix.com",
         user="oa3admin",
         password="Lanix2012$",
         database="oa3_db",
         charset='utf8'
-        
+    )
+    return mydb
+
+def conectar_test():
+    mydb = mysql.connector.connect(   
         #db pruebas
-        # host="sql9.freemysqlhosting.net",
-        # user="sql9590720",
-        # password="4W5iMJJxAI",
-        # database="sql9590720",
-        # charset='latin1'
-=======
-        # Production DB
-        # host="curso-php.com",
-        # user="root_db",
-        # password="Lanix2012$",
-        # database="sql9590720",
-        # charset='latin1'
-        # testing DB
         host="sql9.freemysqlhosting.net",
         user="sql9590720",
         password="4W5iMJJxAI",
         database="sql9590720",
-        charset='latin1'
->>>>>>> 459fef868d38d99efec80619394fde80ae77afdd
+        charset='latin1',
     )
     return mydb
-
